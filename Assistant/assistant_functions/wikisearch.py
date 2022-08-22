6649f8414d337531e02035758ca8b00421c3cd56
ItzSimplyJoe <joebostock30@gmail.com>
2022-08-22 23:31:20 +0100

    Update wikisearch.py

--- Assistant/assistant_functions/wikisearch.py
+++ Assistant/assistant_functions/wikisearch.py
@@ -1,5 +1,5 @@
-#from assistant_functions.speak_listen import speak_listen
-#from assistant_functions.determine_most_similar import determine_most_similar_phrase
+from assistant_functions.speak_listen import speak_listen
+from assistant_functions.determine_most_similar import determine_most_similar_phrase
 import wikipedia
 
 class wikisearch:
@@ -8,23 +8,18 @@ class wikisearch:
 
 
     def search(self,text):
-        print (text)
-        while True:
-            try:
-                result = wikipedia.summary(text)
-                print(result)
-                break
-            except:
-                result = wikipedia.search(text)
-                print(result)
-                #speak_listen.say("Sorry, did you mean "+ result)
-                continue
+        try:
+            result = wikipedia.summary(text, sentences = 2)
+            speak_listen.say(result)
+        except:
+            result = wikipedia.suggest(text)
+            speak_listen.say("Sorry, did you mean "+ result)
+            self.search(text)
 
 
     def keyword(self,text):
         text = text.lower()
         splitstring = text.split()
-        print(splitstring)
         string = ""
         for word in splitstring:
             if word != "what" and word !="when" and word !="how" and word !="where" and word !="a" and word !="is" and word !="who" and word !="was":
@@ -33,10 +28,4 @@ class wikisearch:
         
 
 
-    def output(self,text):
-        print ("!")
-
-
-text = input("enter stuff: ")
 wikisearch = wikisearch()
-wikisearch.keyword(text)
